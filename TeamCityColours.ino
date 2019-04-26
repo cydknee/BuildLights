47c237ee08b3600b25554767ee12e9c3325ac191
cydknee <cydknee@users.noreply.github.com>
2019-04-26 19:23:09 +0100

    Tidied up code

--- TeamCityColours.ino
+++ TeamCityColours.ino
@@ -1,27 +1,25 @@
 #include "FastLED.h"
 
-FASTLED_USING_NAMESPACE
-
-#if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
-#warning "Requires FastLED 3.1 or later; check github for latest code."
-#endif
-
 #define DATA_PIN    3
 #define LED_TYPE    WS2811
 #define COLOR_ORDER RGB
 #define NUM_LEDS    50
-CRGB leds[NUM_LEDS];
-
-#define BRIGHTNESS          10
+#define DELAY_TIME  9500 // 9.5 sec
+#define BRIGHTNESS  10
 #define FRAMES_PER_SECOND  120
 
-String receiveVal = "";     
-unsigned long delayStart = 0; // the time the delay started
+enum colourMode {
+  rainbow,
+  red,
+  green
+};
+
+CRGB leds[NUM_LEDS];   
+char receiveVal[8];
+uint16_t delayStart = 0; // the time the delay started
 bool delayRunning = false; // true if still waiting for delay to finish
-unsigned long DELAY_TIME = 9500; // 9.5 sec
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
-bool showRainbow = true;
-bool showError = false;
+colourMode mode = rainbow;
 
 void setup() 
 {
@@ -29,53 +27,44 @@ void setup()
   
   // tell FastLED about the LED strip configuration
   FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-
   FastLED.setBrightness(BRIGHTNESS);
-
-  delayStart = millis();   // start delay
-  delayRunning = false; // not finished yet
 }
   
 void loop() 
 {  
-    if (showRainbow)
-    {
-      rainbowLights();
-    }
-    else if (showError)
-    {
+  switch(mode) {
+    case red:
       FastLED.showColor(CRGB::Red);
-    }
-    else
-    {
+      break;
+    case green:
       FastLED.showColor(CRGB::Green);
-    }
+      break;
+    default:
+      rainbowLights();
+  }
 
-    if(Serial.available() > 0)  
-    { 
-      receiveVal = Serial.readString(); 
+  if(Serial.available() > 0)  
+  { 
+    Serial.readString().toCharArray(receiveVal,8); 
 
-      if (receiveVal == "g" || receiveVal == "SUCCESS")
-      {
-        showRainbow = false;
-        showError = false;
-
-        delayStart = millis();
-        delayRunning = true;
-      }
-      else if (receiveVal == "r" || receiveVal == "FAILURE")
-      {
-        showRainbow = false;
-        showError = true;
-        FastLED.showColor(CRGB::Red);
-      }
-    }
-
-    if (delayRunning && ((millis() - delayStart) >= DELAY_TIME)) 
+    if (strcmp(receiveVal,"SUCCESS")==0)
     {
-      delayRunning = false;
-      showRainbow = true;
+      mode = green;
+
+      delayStart = millis();
+      delayRunning = true;
     }
+    else if (strcmp(receiveVal,"FAILURE")==0)
+    {
+      mode = red;
+    }
+  }
+
+  if (delayRunning && ((millis() - delayStart) >= DELAY_TIME)) 
+  {
+    delayRunning = false;
+    mode = rainbow;
+  }
 }
 
 void rainbowLights()
